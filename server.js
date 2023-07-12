f7c982f1a4f68cd8f0c42f812807f27ff9234e81
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-12 22:33:00 +0200

    add mongoose

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const socket = require("socket.io");
 const path = require("path");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+
 const testimonialsRoutes = require("./routes/testimonials.routes");
 const concertsRoutes = require("./routes/concerts.routes");
 const seatsRoutes = require("./routes/seats.routes");
@@ -27,6 +29,19 @@ app.get("*", (req, res) => {
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
+
+mongoose.connect("mongodb://localhost:27017/NewWaveDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Connected to the database");
+});
+
+db.on("error", (err) => console.log("Error " + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running on port: 8000");
