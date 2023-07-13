6e454347e7b8a589b82c1d5bcade8f87b7d56764
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-13 15:36:27 +0200

    add methods and controller to testimonials

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,76 +1,16 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
-const db = require("./../db");
+const TestimonialController = require("../controllers/testimonials.controller");
 
-const randomElement = (arr) => {
-  const number = Math.floor(Math.random() * arr.length);
-  return number.toString();
-};
+router.get("/testimonials", TestimonialController.getAll);
 
-router.route("/testimonials").get((req, res) => {
-  res.json(db.testimonials);
-});
+router.get("/testimonials/random", TestimonialController.getRandom);
 
-router.route("/testimonials/random").get((req, res) => {
-  const element = db.testimonials[randomElement(db.testimonials)];
-  res.json(element);
-});
+router.get("/testimonials/:id", TestimonialController.getById);
 
-router.route("/testimonials/:id").get((req, res) => {
-  const element = db.testimonials.find((elem) => elem.id === req.params.id);
-  if (element) {
-    res.json(element);
-  } else {
-    res.json({ message: "Element not found" });
-  }
-});
+router.post("/testimonials", TestimonialController.addNew);
 
-router.route("/testimonials").post((req, res) => {
-  const { author, text } = req.body;
+router.put("/testimonials/:id", TestimonialController.update);
 
-  const newElement = {
-    id: uuidv4(),
-    author,
-    text,
-  };
-
-  if (author && text) {
-    db.testimonials.push(newElement);
-    res.json({ message: "OK" });
-  } else {
-    res.status(200).json({ message: "Error validation" });
-  }
-});
-
-router.route("/testimonials/:id").put((req, res) => {
-  const { author, text } = req.body;
-
-  const element = db.testimonials.find((elem) => elem.id === req.params.id);
-  if (author && text) {
-    if (element) {
-      element.author = author;
-      element.text = text;
-
-      res.json({ message: "OK" });
-    } else {
-      res.status(404).json({ message: "Not found" });
-    }
-  } else {
-    res.status(200).json({ message: "Error validation" });
-  }
-});
-
-router.route("/testimonials/:id").delete((req, res) => {
-  const elementIndex = db.testimonials.findIndex(
-    (elem) => elem.id === req.params.id
-  );
-  if (elementIndex !== -1) {
-    db.testimonials.splice(elementIndex, 1);
-    res.json({ message: "OK" });
-  } else {
-    res.status(404).json({ message: "Not found" });
-  }
-});
-
+router.delete("/testimonials/:id", TestimonialController.delete);
 module.exports = router;
