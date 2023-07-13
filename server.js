b8a76bfd251062bb0b43e1a402b8e5148a0ba8eb
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-13 20:46:14 +0200

    add mongo atlas

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 const testimonialsRoutes = require("./routes/testimonials.routes");
 const concertsRoutes = require("./routes/concerts.routes");
 const seatsRoutes = require("./routes/seats.routes");
-
+const uri =
+  "mongodb+srv://lukasz:Kodilla2023@cluster0.sjmpwid.mongodb.net/NewWaveDB?retryWrites=true&w=majority";
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -30,7 +31,7 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-mongoose.connect("mongodb://localhost:27017/NewWaveDB", {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
